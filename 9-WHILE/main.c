7cc759d15d5079843bf50ce7b381b608cc431cac
JAreina <jareinafdez@gmail.com>
2018-06-17 23:16:49 +0200

    WHILE DOS

--- 9-WHILE/main.c
+++ 9-WHILE/main.c
@@ -5,5 +5,8 @@ int main(int argc, char **argv)
 	char carac;
     carac = '\0';
     while( carac != 'A') carac = getchar();
+	
+	while ( (carac = getchar()) != 'B' )
+           putchar(carac);
 	return 0;
 }
