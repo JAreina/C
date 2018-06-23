91a3911db9eb24f7d8545e61bb16c1054a027937
JAreina <jareinafdez@gmail.com>
2018-06-23 08:46:38 +0200

    file

--- 23-FILE/main.c
+++ 23-FILE/main.c
@@ -5,11 +5,19 @@ int main(int argc, char **argv)
 	
 	FILE *archivo;
 	int cerrado;
+	int i;
 	
-	if((archivo = fopen("fichero1","w+"))== NULL);
+	if((archivo = fopen("fichero1","a"))== NULL);
 	perror( "No se puede abrir el fichero\n");
+	
+	// escribir
+	for(i =0;i<6;i++)
+	   fprintf(archivo, "estoy escribiendo\n");
+	
+	
 	
 	cerrado = fclose(archivo);//devuelve 0 en caso de éxito y EOF en caso de error
 	printf("%d\n",cerrado);
+	rename("fichero1","RENOMBRADO.txt");
 	return 0;
 }
